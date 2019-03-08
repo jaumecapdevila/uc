5836167074aeacf742035266d3061aba30073dd5
Jaume Capdevila <contact@jaumecapdevila.net>
2019-03-08 17:25:11 +0100

    Display hello world

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
-  // Create the browser window.
   let win = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the index.html of the app.
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "pages/index.html"));
 }
 
 app.on("ready", createWindow);
