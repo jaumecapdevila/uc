74196513aad2807409b993f7aa0192aee1fe3989
Jaume Capdevila <contact@jaumecapdevila.net>
2019-03-08 17:20:57 +0100

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: 'babel-eslint', // Specifies the ESLint parser
+  extends: [
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
